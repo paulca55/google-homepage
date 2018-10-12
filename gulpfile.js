2455d86d0f284bd0f8ca96dc471255392e509c71
Paul Cass <7057526+paulca55@users.noreply.github.com>
2018-10-12 18:29:04 +0100

    Added gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,39 @@
 // Load plugins
 var gulp = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer');
+  browserSync = require('browser-sync').create(),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer');
 
 // Theme stylesheet (development)
 gulp.task('styles', function() {
-    return gulp
-        .src('./scss/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-        .pipe(postcss([autoprefixer()]))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./'))
-        .pipe(browserSync.stream());
+  return gulp
+    .src('./scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
 });
 
 // Browsersync
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
 });
 
 // Watch
 gulp.task('watch', function() {
-    gulp.watch('scss/style.scss', ['styles']);
+  gulp.watch('scss/style.scss', ['styles']);
 });
 
 // Default gulp task
 gulp.task('default', ['styles', 'browser-sync', 'watch']);
+
+// Build
+gulp.task('build', ['styles']);
